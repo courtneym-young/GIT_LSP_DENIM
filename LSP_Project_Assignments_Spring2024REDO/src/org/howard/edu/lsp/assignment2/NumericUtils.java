936a65d22b3f25c002d0822c54dac3543ec7f01e
Courtney <courtneymac2003@gmail.com>
2024-02-29 19:51:18 -0500

    This is assignment #2.

---
+++ LSP_Project_Assignments_Spring2024REDO/src/org/howard/edu/lsp/assignment2/NumericUtils.java
@@ -0,0 +1,18 @@
+/*
+ * Name: Courtney Young
+ */
+package org.howard.edu.lsp.assignment2;
+
+public class NumericUtils {
+
+	public static boolean isNumeric(String str) {
+		try {
+			Double.parseDouble(str);
+			return true;
+		} catch (NumberFormatException e) {
+			return false;
+		}
+
+	}
+
+}
---
+++ LSP_Project_Assignments_Spring2024REDO/src/org/howard/edu/lsp/assignment2/WordCounting.java
@@ -0,0 +1,39 @@
+/*
+ * Name: Courtney Young
+ */
+package org.howard.edu.lsp.assignment2;
+
+import java.io.*;
+import java.util.*;
+
+public class WordCounting {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String filename = "src/org/howard/edu/lsp/assignment2/words.txt"; //Relative path for file
+		HashMap<String, Integer> wordCounts = new HashMap<>();
+		
+		try (BufferedReader br = new BufferedReader(new FileReader(filename))) {
+			String line;
+			while ((line = br.readLine()) != null) {
+				line = line.replaceAll("\\p{Punct}", " "); //Replace all punctuation with whitespace
+				String[] words = line.split("[\\s+]"); //Split line into words using whitespace as delimiter
+				for (String word : words) {
+					word = word.replaceAll("[^a-zA-Z']", "").toLowerCase(); //Remove non-letter characters and convert to lowercase
+					if (word.length() > 3 && !NumericUtils.isNumeric(word)) { //Ignore trivial words and numeric strings
+						wordCounts.put(word,  wordCounts.getOrDefault(word, 0) + 1);
+					}
+				}
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		System.out.println("Number of times each word appears in the text file:"); //Echo the input file
+		
+		for (HashMap.Entry<String, Integer> entry : wordCounts.entrySet()) {
+			System.out.println(entry.getKey() + " " + entry.getValue());
+		}
+	}
+
+}
