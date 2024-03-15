889b37b27529193dba3c53cfd35face404db789c
Courtney <courtneymac2003@gmail.com>
2024-03-14 20:29:35 -0400

    Added JAVADOCS

--- LSP_Project_Assignments_Spring2024REDO/src/org/howard/edu/lsp/midterm/question2/Range.java
+++ LSP_Project_Assignments_Spring2024REDO/src/org/howard/edu/lsp/midterm/question2/Range.java
@@ -1,18 +1,44 @@
 package org.howard.edu.lsp.midterm.question2;
 
+/**
+ * Interface representing a range of integers
+ */
 public interface Range {
 	
-	//Returns true if v is >= lower bound and <= upper bound, and false otherwise
+	/**
+	 * Checks if the range contains a specific value
+	 * 
+	 * @param value the value to check
+	 * @return true if the range contains the value, false otherwise
+	 */
 	public boolean contains(int value);
 	
-	//Returns true if the receiver contains at least
-	//one value in common with other, and false otherwise
+	/**
+	 * Checks if the range overlaps with another range
+	 * 
+	 * @param other the other range to check with for overlapping
+	 * @return true if there is an overlap, false otherwise
+	 */
 	public boolean overlaps(Range other);
 	
-	//Returns the number of integers in the range
+	/**
+	 * Calculates the number of integers in the range
+	 * 
+	 * @return the size of the range
+	 */
 	public int size();
 
+	/**
+	 * Gets the lower bound of the range
+	 * 
+	 * @return the lower bound
+	 */
 	public int getLowerBound();
 	
+	/**
+	 * Gets the upper bound of the range
+	 * 
+	 * @return the upper bound
+	 */
 	public int getUpperBound();
 }
