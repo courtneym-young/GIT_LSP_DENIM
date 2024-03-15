0094bfea7225ad8a0c9bf597c559d9ce7bfe6bb0
Courtney <courtneymac2003@gmail.com>
2024-03-14 20:08:22 -0400

    Rename temp file

--- LSP_Project_Assignments_Spring2024REDO/src/org/howard/edu/lsp/midterm/question2/IntegerRange.java
+++ LSP_Project_Assignments_Spring2024REDO/src/org/howard/edu/lsp/midterm/question2/IntegerRange.java
@@ -1,6 +1,6 @@
 package org.howard.edu.lsp.midterm.question2;
 
-public class Q2Temp {
+public class IntegerRange {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
