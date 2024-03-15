810007836f9a08fd7de59511efc7125750b66afa
Courtney <courtneymac2003@gmail.com>
2024-03-14 20:00:16 -0400

    Add in code from newly named file

--- LSP_Project_Assignments_Spring2024REDO/src/org/howard/edu/lsp/midterm/question1/SecurityOps.java
+++ LSP_Project_Assignments_Spring2024REDO/src/org/howard/edu/lsp/midterm/question1/SecurityOps.java
@@ -1,10 +1,37 @@
 package org.howard.edu.lsp.midterm.question1;
 
-public class Q1Temp {
+/**
+ * Provides methods for text encryption
+ */
+public class SecurityOps {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	/**
+	 * Encrypts the given text using a simple algorithm
+	 * 
+	 * @param text the text to be encrypted
+	 * @return the encrypted text
+	 */
+	public static String encrypt(String text) {
+		
+		String cleanText = text.replaceAll("[\\s\\p{Punct}]", ""); //Remove whitespace and punctuation from the input text
+		
+		String evenChars = ""; //Initialize string to hold even index characters
+		String oddChars = ""; //Initialize string to hold odd index characters
+		
+		for (int i = 0; i < cleanText.length(); i++) //Iterate through the text to separate even and odd characters
+		{
+			char currentChar = cleanText.charAt(i);
+			if (i % 2 == 0) 
+			{
+				evenChars += currentChar;
+			}
+			else
+			{
+				oddChars += currentChar;
+			}
+		}
+		
+		return evenChars + oddChars; //Return the combination of even and odd characters
 	}
 
 }
