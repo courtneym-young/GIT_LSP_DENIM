7a558374e7839e99d0c1649fbeb66c6c17e026b7
Courtney <courtneymac2003@gmail.com>
2024-03-14 20:42:31 -0400

    Testing file completed

--- LSP_Project_Assignments_Spring2024REDO/src/org/howard/edu/lsp/midterm/question2/RangeTester.java
+++ LSP_Project_Assignments_Spring2024REDO/src/org/howard/edu/lsp/midterm/question2/RangeTester.java
@@ -1,9 +1,28 @@
 package org.howard.edu.lsp.midterm.question2;
 
+/**
+ * A class to test the functionality of IntegerRange
+ */
 public class RangeTester {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		//create two integer ranges
+		IntegerRange range1 = new IntegerRange(1, 10);
+		IntegerRange range2 = new IntegerRange(5, 15);
+		
+		//Testing contains method
+		System.out.println("Does range1 contain 5? " + range1.contains(5));
+		System.out.println("Does range1 contain 15? " + range1.contains(15));
+		
+		//Testing overlaps method
+		System.out.println("Do range1 and range2 overlap? " + range1.overlaps(range2));
+		
+		//Testing size method
+		System.out.println("Size of range1: " + range1.size());
+		
+		//Testing equals method
+		IntegerRange range3 = new IntegerRange(1, 10);
+		System.out.println("Are range1 and range3 equal? " + range1.equals(range3));
 
 	}
 
